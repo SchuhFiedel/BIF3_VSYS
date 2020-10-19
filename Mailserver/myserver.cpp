a11b2392be0cdaa7fa92fad5b02ffd42c52a5b59
Maximiliano Zulli <maximiliano.zulli@gmail.com>
2020-10-19 17:19:37 +0200

    Basic Thread Functionality works (we have to find a better way to make these threads!)

--- Mailserver/myserver.cpp
+++ Mailserver/myserver.cpp
@@ -3,7 +3,6 @@
 #include <bits/stdc++.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -12,44 +11,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <iostream>
 
+#include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstdlib>
+#include <thread>
+#include <mutex>
 
 #include <dirent.h>
+#include <pthread.h>
+
 
 
 #define BUF 1024
 #define PORT 6543
+#define MAX_CLIENTS 5
 
 using namespace std;
 
-std::string user = "";
 std::string mail_path = "";
-std::string user_mail_path = "";
 
-void receiveMsg(int new_socket);
-void deleteMail(int new_socket);
-void readmsg(int new_socket);
-void listmsg(int new_socket);
+void receiveMsg(int new_socket, std::string user_mail_path);
+void deleteMail(int new_socket, std::string user_mail_path);
+void readmsg(int new_socket, std::string user_mail_path);
+void listmsg(int new_socket, std::string user_mail_path);
+int connectionLoop(struct sockaddr_in address, int create_socket, char * mailpath);
 
 int main (int argc, char **argv)
 {
-    int create_socket, new_socket;
-    socklen_t addrlen; //int 32 bit
-    char buffer[BUF];   //string with 1024 characters
-    int size;
-    struct sockaddr_in address, cliaddress; //makes two structs with adress, port and adress_family (AF_INET)
-    int setup = 0;
-
+    int create_socket;
+    struct sockaddr_in address; //makes struct with adress, port and adress_family (AF_INET)
     if( argc < 3 )
     {
         printf("Usage: %s PortNumber MailFolder (PATH)\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     char * mailpath = argv[2];
-
     create_socket = socket (AF_INET, SOCK_STREAM, 0);
 
     memset(&address,0,sizeof(address)); //fill with 0
@@ -58,153 +56,167 @@ int main (int argc, char **argv)
     address.sin_port = htons(atoi(argv[1]));
 
     /*     bind() assigns
-    the address specified by addr to the socket referred to by the file
-    descriptor sockfd.  addrlen specifies the size, in bytes, of the
-    address structure pointed to by addr.  Traditionally, this operation
-    is called “assigning a name to a socket”.*/
+    the address specified by addr to the socket referred to by the file descriptor sockfd.  addrlen specifies the size, in bytes, of the address structure pointed to by addr.  Traditionally, this operation is called “assigning a name to a socket”.
+    */
 
-    if (bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0)
-    {
+    if (bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0){
         perror("bind error");
         return EXIT_FAILURE;
     }
 
-    listen (create_socket, 5); //Socket to listen to and maximal connection Queue lenght
-    addrlen = sizeof (struct sockaddr_in);
-    bool end = false;
+    listen (create_socket, MAX_CLIENTS); //Socket to listen to and maximal connection Queue lenght
 
-    while (end == false)
-    {
-        std::cout << "Waiting for Connection on: IP: " << inet_ntoa(address.sin_addr) << " Port: " << htons(address.sin_port) << "\n";
-        new_socket = accept ( create_socket, (struct sockaddr *) &cliaddress, &addrlen );
-        if (new_socket > 0)
-        {
-            printf ("Client connected from %s:%d...\n", inet_ntoa (cliaddress.sin_addr), ntohs(cliaddress.sin_port));
-            strcpy(buffer,"Welcome to myserver, Please enter your command:\n");
-            send(new_socket, buffer, strlen(buffer),0);
-        }
-        do
-        {
-            cout << "AAA" << endl;
-            size = recv (new_socket, buffer, BUF-1, 0);
-            if( size > 0)
-            {
-                cout << "BBB" << endl;
-                buffer[size] = '\0';
-                if(setup == 0){
-                  cout << "CCC" << endl;
-                  //Create Direcotry with -- later uSERNAME
-                    std::string pathPartOne(mailpath);
-                    std::cout<< "pathpartOne: " << pathPartOne << endl;
-                    std::string pathpartTwo(buffer);
-                    std::cout<< "pathpartTwo: " << pathpartTwo << endl;
-                    std::string path = pathPartOne + "/" + pathpartTwo;
-                    std::cout<< "PATH: " << path << endl;
-                    mail_path = pathPartOne;
-                    std::cout<< "mail_path: " << mail_path << endl;
-                    user = pathpartTwo;
-                    std::cout << "User: "<< user << endl;
-                    user_mail_path = path;
-                    std::cout<< "user_mail_path: " << user_mail_path << endl;
+//THREAD STUFF
+    //pthread_t threads[MAX_CLIENTS];
 
+      std::cout << "Creating a thread!!!! " << std::endl;
+      std::thread th0(std::bind(connectionLoop, address , create_socket, mailpath));
+      std::thread th1(std::bind(connectionLoop, address , create_socket, mailpath));
+      std::thread th2(std::bind(connectionLoop, address , create_socket, mailpath));
+      std::thread th3(std::bind(connectionLoop, address , create_socket, mailpath));
+      std::thread th4(std::bind(connectionLoop, address , create_socket, mailpath));
+      th0.join();
+      th1.join();
+      th2.join();
+      th3.join();
+      th4.join();
+    
 
-                    mkdir(pathPartOne.c_str(), 0777);
-
-                    if(mkdir(path.c_str(), 0777) == -1)
-                    {
-                        std::cerr << "Error :  " << strerror(errno) << std::endl;
-                    }
-                    else
-                    {
-                        std::cout << "Directory "<< buffer <<" created";
-                    }
-                    setup = 1;
-
-                }
-
-                //Defining Stuff that has to be checked
-                char SEND = 'S';
-                char LIST = 'L';
-                char READ = 'R';
-                char DEL = 'D';
-                char QUIT = 'Q';
-
-                if(buffer[0] == SEND)
-                {
-                    std::cout<<"Send received" << std::endl << std::endl;
-                    //std::cout<<"THIS MESSAGE WAS IN THE receive function: "<<buffer<<std::endl;
-                    receiveMsg(new_socket);
-                }
-                else if(buffer[0] == LIST)
-                {
-                    std::cout<<"LIST received";
-                    listmsg(new_socket);
-                }
-                else if(buffer[0] == READ)
-                {
-                    std::cout<<"READ received";
-                    readmsg(new_socket);
-                }
-                else if(buffer[0] == DEL)
-                {
-                    std::cout<<"DEL received";
-                    deleteMail(new_socket);
-                }
-                else if(buffer[0] == QUIT)
-                {
-                    setup = 0;
-                    close (new_socket);
-                    end = true;
-                    user = "";
-                    mail_path = "";
-                    user_mail_path = "";
-                }
-
-
-                //printf ("Message received: %s\n", buffer);
-
-            }
-            else if (size == 0)
-            {
-                setup = 0;
-                printf("Client closed remote socket\n");
-                close (new_socket);
-                //close (create_socket);
-                user = "";
-                mail_path = "";
-                user_mail_path = "";
-                break;
-            }
-            else
-            {
-                setup = 0;
-                perror("recv error");
-                user = "";
-                mail_path = "";
-                user_mail_path = "";
-                return EXIT_FAILURE;
-            }
-        }
-        while (strncmp (buffer, "Q", 1)  != 0);
-        close (new_socket);
-        setup = 0;
-        //close (create_socket);
-        user = "";
-        mail_path = "";
-        user_mail_path = "";
-    }
+//END THREAD STUFF
     close (create_socket);
-    setup = 0;
-    user = "";
-    mail_path = "";
-    user_mail_path = "";
     return EXIT_SUCCESS;
 }
 
+//MAIN LOOP HERE
+int connectionLoop(struct sockaddr_in address, int create_socket, char * mailpath){
+  std::string user_mail_path = "";
+  std::string user = "";
+  bool end = false;
+  int new_socket;
+  int setup = 0;
+  int size;
+  char buffer[BUF];   //string with 1024 characters
+  socklen_t addrlen = sizeof (struct sockaddr_in); //int 32 bit
+  struct sockaddr_in cliaddress;
+
+  while (end == false)
+  {
+      std::cout << "Waiting for Connection on: IP: " << inet_ntoa(address.sin_addr) << " Port: " << htons(address.sin_port) << "\n";
+      new_socket = accept ( create_socket, (struct sockaddr *) &cliaddress, &addrlen );
+      if (new_socket > 0)
+      {
+          printf ("Client connected from %s:%d...\n", inet_ntoa (cliaddress.sin_addr), ntohs(cliaddress.sin_port));
+          strcpy(buffer,"Welcome to myserver, Please enter your command:\n");
+          send(new_socket, buffer, strlen(buffer),0);
+      }
+      do
+      {
+          cout << "AAA" << endl;
+          size = recv (new_socket, buffer, BUF-1, 0);
+          if( size > 0)
+          {
+              cout << "BBB" << endl;
+              buffer[size] = '\0';
+              if(setup == 0){
+                cout << "CCC" << endl;
+                //Create Direcotry with -- later uSERNAME
+                  std::string pathPartOne(mailpath);
+                  std::cout<< "pathpartOne: " << pathPartOne << endl;
+                  std::string pathpartTwo(buffer);
+                  std::cout<< "pathpartTwo: " << pathpartTwo << endl;
+                  std::string path = pathPartOne + "/" + pathpartTwo;
+                  std::cout<< "PATH: " << path << endl;
+                  mail_path = pathPartOne;
+                  std::cout<< "mail_path: " << mail_path << endl;
+                  user = pathpartTwo;
+                  std::cout << "User: "<< user << endl;
+                  user_mail_path = path;
+                  std::cout<< "user_mail_path: " << user_mail_path << endl;
 
 
+                  mkdir(pathPartOne.c_str(), 0777);
 
-void receiveMsg(int new_socket){
+                  if(mkdir(path.c_str(), 0777) == -1)
+                  {
+                      std::cerr << "Error :  " << strerror(errno) << std::endl;
+                  }
+                  else
+                  {
+                      std::cout << "Directory "<< buffer <<" created";
+                  }
+                  setup = 1;
+              }
+              //Defining Stuff that has to be checked
+              char SEND = 'S';
+              char LIST = 'L';
+              char READ = 'R';
+              char DEL = 'D';
+              char QUIT = 'Q';
+
+              if(buffer[0] == SEND)
+              {
+                  std::cout<<"Send received" << std::endl << std::endl;
+                  //std::cout<<"THIS MESSAGE WAS IN THE receive function: "<<buffer<<std::endl;
+                  receiveMsg(new_socket, user_mail_path);
+              }
+              else if(buffer[0] == LIST)
+              {
+                  std::cout<<"LIST received";
+                  listmsg(new_socket, user_mail_path);
+              }
+              else if(buffer[0] == READ)
+              {
+                  std::cout<<"READ received";
+                  readmsg(new_socket, user_mail_path);
+              }
+              else if(buffer[0] == DEL)
+              {
+                  std::cout<<"DEL received";
+                  deleteMail(new_socket, user_mail_path);
+              }
+              else if(buffer[0] == QUIT)
+              {
+                  setup = 0;
+                  close (new_socket);
+                  end = true;
+                  user = "";
+                  mail_path = "";
+                  user_mail_path = "";
+              }
+          }
+          else if (size == 0)
+          {
+              setup = 0;
+              printf("Client closed remote socket\n");
+              close (new_socket);
+              //close (create_socket);
+              user = "";
+              mail_path = "";
+              user_mail_path = "";
+              break;
+          }
+          else
+          {
+              setup = 0;
+              perror("recv error");
+              user = "";
+              mail_path = "";
+              user_mail_path = "";
+              return EXIT_FAILURE;
+          }
+      }
+      while (strncmp (buffer, "Q", 1)  != 0);
+      close (new_socket);
+      setup = 0;
+      //close (create_socket);
+      user = "";
+      mail_path = "";
+      user_mail_path = "";
+  }
+}
+
+
+void receiveMsg(int new_socket, std::string user_mail_path){
 
     char buffer[BUF]= "";   //string with 1024 characters
     int size;
@@ -279,7 +291,7 @@ void receiveMsg(int new_socket){
 
 }
 
-void deleteMail(int new_socket){
+void deleteMail(int new_socket, std::string user_mail_path){
   //receives which mail to delete
   char buffer[BUF] = "";    //string with 1024 characters
   int size;
@@ -302,7 +314,7 @@ void deleteMail(int new_socket){
 }
 
 
-void readmsg(int new_socket){
+void readmsg(int new_socket, std::string user_mail_path){
   //receives which mail to open
   char  reply [4];
   char buffer[BUF] = "";   //string with 1024 characters
@@ -339,12 +351,12 @@ void readmsg(int new_socket){
 
 }
 
-void listmsg(int new_socket){
+void listmsg(int new_socket, std::string user_mail_path){
   char buffer[BUF] = "";
   //read stuff out of file
   //std::string file_to_read = user_mail_path;
   std::string all_entries = "";
-  std::cout << "User: "<< user << endl;
+  //std::cout << "User: "<< user << endl;
 
   struct dirent * entry = nullptr;
   DIR * dp = nullptr;
--- makefile
+++ makefile
@@ -4,11 +4,11 @@ all: server client
 
 server: Mailserver/myserver.cpp
 	@echo "Making Server Vsys 1 - myserver.c binary!"
-	g++  Mailserver/myserver.cpp -o Mailserver/myserver.o
+	g++  -pthread Mailserver/myserver.cpp -o Mailserver/myserver.o
 
 client: Mailclient/myclient.cpp
 	@echo "Making Client Vsys1 - myclient.c binary!"
-	g++  Mailclient/myclient.cpp -o Mailclient/myclient.o
+	g++  -pthread Mailclient/myclient.cpp -o Mailclient/myclient.o
 
 clean:
 	rm Mailclient/*.o 
